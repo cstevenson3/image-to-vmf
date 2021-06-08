2ac85d96939dd74321382937abf34f3433681890
Cameron Stevenson <cameron.stevenson03@gmail.com>
2021-06-08 13:02:38 +1200

    Removed unused functions

--- src/sketch.py
+++ src/sketch.py
@@ -16,14 +16,6 @@ def display(img):
         cv2.destroyAllWindows()
         cv2.waitKey(1)
     sleep(1)
-    
-def range_minus_255_to_255(image):
-    img = image.copy()
-    img = 2 * img
-    for y in range(len(img)):
-        for x in range(len(img[0])):
-            img[y][x] -= 255
-    return img
 
 def convolve_symbols(img, symbols, width=128, height=128):
     image = img.copy()
@@ -185,33 +177,6 @@ def get_text(img, texts = ["A"]):
 
     return result
 
-def find_edge_ends(bin_img, dilation_iterations = 1):
-    ret, thresh = cv2.threshold(bin_img, 128, 255, cv2.THRESH_BINARY)
-    # set white pixels to 10, black pixels to 0
-    reshade_kernel = np.array([[10.0/255.0]])
-    reshade = cv2.filter2D(thresh, -1, reshade_kernel)
-
-    # slow method for reshade
-    # for i in range(len(thresh)):
-    #     for j in range(len(thresh[0])):
-    #         if thresh[i][j] == 255:
-    #             thresh[i][j] = 10
-    #         else:
-    #             thresh[i][j] = 0
-
-    # convolve with kernel which gives 110 as output
-    # for white pixels with exactly one white neighbour
-    edge_end_kernel = np.array([[1, 1, 1], [1, 10, 1], [1, 1, 1]])
-    edge_end_110 = cv2.filter2D(reshade, -1, edge_end_kernel)
-    edge_end_thresh = cv2.inRange(edge_end_110, 109, 111)
-
-    kernel = np.ones((11,11), np.uint8)
-    dil = cv2.dilate(edge_end_thresh, kernel, iterations = dilation_iterations)
-
-    #display(dil)
-
-    return dil
-
 def scale(img, scale, interpolation=cv2.INTER_NEAREST):
     result = cv2.resize(img, (int(scale * len(img[0])), int(scale * len(img))), interpolation=interpolation)
     return result
@@ -228,18 +193,6 @@ def adaptive_threshold(img, size=11):
 
    img = adp
    return img
-
-def xy_edges(img):
-    dx_kernel = 9 * np.array([[-1,0,1], [-1,0,1], [-1,0,1]])
-    dx_edges = cv2.filter2D(img, -1, dx_kernel)
-
-    dy_kernel = np.transpose(dx_kernel)
-    dy_edges = cv2.filter2D(img, -1, dy_kernel)
-
-    both_edges = cv2.addWeighted(dx_edges, 0.5, dy_edges, 0.5, 0)
-    
-    img = both_edges
-    return img
 
 def close(img, size=5, dilations=1, erosions=1):
     kernel = np.ones((size, size), np.uint8)
@@ -322,96 +275,6 @@ def get_pixel_regions(img):
     # output = draw_contours(fex_inv, c)
     return(adp_closed)
 
-def get_borders(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    invert = cv2.bitwise_not(gray)
-
-    length = 15
-    long_x_kernel = np.array([[-1] * length, [0] * length, [0] * length, [2] * length, [0] * length, [0] * length, [-1] * length])
-    long_x_edge = cv2.filter2D(invert, -1, long_x_kernel)
-
-    long_y_kernel = np.transpose(long_x_kernel)
-    long_y_edge = cv2.filter2D(invert, -1, long_y_kernel)
-
-    sobel = cv2.Sobel(invert, -1, 1, 1, ksize=5)
-    ret, sobel_thresh = cv2.threshold(sobel, 50, 255, cv2.THRESH_BINARY)
-
-    overlap = cv2.addWeighted(long_x_edge, 0.5, long_y_edge, 0.5, 0)
-    #overlap = cv2.addWeighted(overlap, 0.5, sobel, 0.5, 0)
-
-    sharp_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    sharp_invert = cv2.filter2D(invert, -1, sharp_kernel)
-
-    blur = cv2.GaussianBlur(sharp_invert, (5,5), 0)
-
-    dx_kernel = 18 * np.array([[-1,0,1], [-1,0,1], [-1,0,1]])
-    dx_edges = cv2.filter2D(invert, -1, dx_kernel)
-
-    dy_kernel = 9 * np.array([[1,1,1], [0,0,0], [-1,-1,-1]])
-    dy_edges = cv2.filter2D(invert, -1, dy_kernel)
-
-    d_edges = cv2.addWeighted(dx_edges, 0.5, dy_edges, 0.5, 0)
-
-    blur_d = cv2.GaussianBlur(d_edges, (7,7), 0)
-    blur_d2 = cv2.GaussianBlur(blur_d, (15,15), 0)
-
-    ret, thresh_d = cv2.threshold(blur_d, 100, 255, cv2.THRESH_BINARY)
-
-    kernel = np.ones((3,3), np.uint8)
-    erode_d = cv2.erode(thresh_d, kernel, iterations=1)
-    dilate_d = cv2.dilate(erode_d, kernel, iterations=4)
-    erode_d2 = cv2.erode(dilate_d, kernel, iterations=1)
-    dilate_d2 = cv2.dilate(erode_d2, kernel, iterations=2)
-    blur_dilate = cv2.GaussianBlur(dilate_d2, (7,7), 0)
-    dilate_d2 = cv2.dilate(blur_dilate, kernel, iterations=1)
-    ret, thresh_dil = cv2.threshold(dilate_d2, 100, 255, cv2.THRESH_BINARY)
-
-    canny_d = cv2.Canny(thresh_dil, 5, 8)
-
-    dilate_canny = cv2.dilate(canny_d, kernel, iterations=1)
-
-    reinvert = cv2.bitwise_not(dilate_canny)
-    contours, hierarchy = cv2.findContours(dilate_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours_drawn = img.copy()
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area < 2000:
-            continue
-        contours_drawn = cv2.drawContours(contours_drawn, [contour], -1, (0,0,255), 2)
-
-    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    sharp = cv2.filter2D(invert, -1, kernel)
-
-    blur = cv2.GaussianBlur(sharp, (15,15), 0)
-
-    ret, thresh = cv2.threshold(blur, 110, 255, cv2.THRESH_BINARY)
-
-    kernel = np.ones((3,3), np.uint8)
-    dilate = cv2.dilate(thresh, kernel, iterations=3)
-
-    blur_morph = dilate
-
-    for iterations in range(10):
-        blur_morph = cv2.GaussianBlur(blur_morph, (15,15), 0)
-
-    dilate2 = cv2.dilate(blur_morph, kernel, iterations=1)
-
-    output = reinvert
-
-    # blur = cv2.GaussianBlur(gray, (15,15), 0)
-    # blur2 = cv2.GaussianBlur(blur, (15,15), 0)
-    # edges = cv2.Canny(blur2, 5, 8)
-    # kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    # sharp = cv2.filter2D(blur2, -1, kernel)
-    # sharp2 = cv2.filter2D(sharp, -1, kernel)
-    # ret, thres = cv2.threshold(sharp, 150, 255, cv2.THRESH_BINARY)
-    # invert = cv2.bitwise_not(thres)
-    # contours, hierarchy = cv2.findContours(thres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # thres = cv2.drawContours(thres, contours, -1, (0,0,255), 2)
-    # kernel = np.ones((3,3), np.uint8)
-    # erosion = cv2.erode(thres, kernel, iterations=3)
-    display(output) if DEBUGGING else None
-
 def get_black_segments(img):
     MIN_AREA = 1000
 
@@ -457,7 +320,6 @@ def get_black_segments(img):
                     #display(result)
     return result
 
-
 def neighbour_colors(img, x, y):
     result = []
     xys = []
@@ -469,7 +331,6 @@ def neighbour_colors(img, x, y):
     for xy in xys:
         result.append(tuple(img[xy[1]][xy[0]]))
     return result
-
 
 def template_match_test():
     img = import_image("tests/test_data/sketch_scanned5.png")
